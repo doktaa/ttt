4cf07b04b38c3368033b9b6a17d3b1b5d533b906
doktaa <normantan85@gmail.com>
2021-04-15 21:57:51 +1200

    Game function completed
    Game function completed, just have to do visual stuff now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Tile from './components/tile.js'
 import ximg from './images/x.png';
 import oimg from './images/o.png';
 
 function App() {
 
-  const [turn, setTurn] = useState('X')
+  const [turn, setTurn] = useState()
   const [tileArray, setTileArray] = useState([null, null, null, null, null, null, null, null, null])
+
+  useEffect(() => {
+    switch(turn) {
+      case 'X':
+        document.getElementById("game-msg").innerHTML = "Player X's Turn"
+        break;
+      case 'O':
+        document.getElementById("game-msg").innerHTML = "Player O's Turn"
+        break;
+    }
+  })
 
 
   function tileClick(num) {
     // alert(num);
-    document.getElementById(num).innerHTML = '<img class="xoimg" src="' + (turn === 'X' ? ximg : oimg) + '"/>'
 
-    setTurn(turn === 'X' ? 'O' : 'X');
+    if (turn === 'X' || turn === 'O') {
+      tileArray[num] = turn;      
+      document.getElementById(num).innerHTML = '<img class="xoimg" src="' + (turn === 'X' ? ximg : oimg) + '"/>'
+
+      console.log(tileArray);
+      let winner = checkMatch()
+      console.log('winner', tileArray, winner);
+
+      if(winner === null) {        
+        setTurn(turn === 'X' ? 'O' : 'X');
+      } else {
+        setTurn(null);
+        document.getElementById("game-msg").innerHTML = `Player ${winner} Wins`;
+      }
+    }
+  }
+
+  function startGame() {
+    setTurn((Math.random * 100) > 50 ? 'X' : 'O');
+  }
+
+  function checkMatch() {
+
+    console.log('HELLO CHECKMATCH TILEARRAY', tileArray);
+    if (tileArray[0] === tileArray[1] && tileArray[1] === tileArray[2] && tileArray[0] !== null){
+      return tileArray[0];
+    } else if (tileArray[3] === tileArray[4] && tileArray[4] === tileArray[5] && tileArray[3] !== null){
+      return tileArray[3]
+    } else if (tileArray[6] === tileArray[7] && tileArray[7] === tileArray[8] && tileArray[6] !== null){
+      return tileArray[6]
+    } else if (tileArray[0] === tileArray[3] && tileArray[3] === tileArray[6] && tileArray[0] !== null){
+      return tileArray[0]
+    } else if (tileArray[1] === tileArray[4] && tileArray[4] === tileArray[7] && tileArray[1] !== null){
+      return tileArray[1]
+    } else if (tileArray[2] === tileArray[5] && tileArray[5] === tileArray[8] && tileArray[2] !== null){
+      return tileArray[2]
+    } else if (tileArray[2] === tileArray[4] && tileArray[4] === tileArray[6] && tileArray[2] !== null){
+      return tileArray[2]
+    } else if (tileArray[0] === tileArray[4] && tileArray[4] === tileArray[8] && tileArray[0] !== null){
+      return tileArray[0];
+    } else {
+      return null;
+    }
   }
 
 
   return (
     <div className="App">
+      <p id="game-msg">Please start new game</p>
       <table className="grid-table">
         <tr className="grid-row">
           <td className="grid-col"><Tile num="0" click={tileClick}/></td>
@@ -38,7 +91,7 @@ function App() {
           <td className="grid-col"><Tile num="8" click={tileClick}/></td>
         </tr>
       </table>
-      <button>Start</button>
+      <button onClick={startGame}>Start</button>
     </div>
   );
 }
